f829eb4dae772d33d066bd9124288527462be5c1
Sergiordo <141227441+Sergiordo@users.noreply.github.com>
2023-11-30 13:14:29 +0700

    Update Test average.py

--- Test average.py
+++ Test average.py
@@ -1,6 +1,6 @@
 def average():
     get_score = int(input("Enter the score: "))
-    final_score = (get_score / 30) * 100
+    final_score = (get_score / 30) * 100    #in this particular instance, the test has got a maximum of 30 points.
     return final_score
 
 score = average()
