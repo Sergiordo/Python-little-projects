816bd10262873055b0b56a6ec71bf9e5884b76dd
Sergio Ordonez Lopez <141227441+Sergiordo@users.noreply.github.com>
2023-12-05 16:07:21 +0700

    palindromes.py

--- palindromes.py
+++ palindromes.py
@@ -1,11 +1,11 @@
 def is_palindrome(string):
     reversed_string = string[::-1]
-    if string == reversed_string:
+    if string.lower() == reversed_string.lower():
         return True
     else:
         print("This is not a palindrome.")
         return False
 
-test = "sggis"
+test = "Anna"
 result = is_palindrome(test)
 print(result)
