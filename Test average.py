881ce301e4d08fe6e951077e22255ada2f949815
Sergio Ordonez Lopez <141227441+Sergiordo@users.noreply.github.com>
2024-03-03 13:29:14 +0700

    Update Test average.py

--- Test average.py
+++ Test average.py
@@ -1,6 +1,7 @@
 def average():
     get_score = int(input("Enter the score: "))
-    final_score = (get_score / 30) * 100    #in this particular instance, the test has got a maximum of 30 points.
+    Total_questions = int(input("Enter the total number of questions: "))
+    final_score = (get_score / Total_questions) * 100    #in this particular instance, the test has got a maximum of 30 points.
     return final_score
 
 score = average()
