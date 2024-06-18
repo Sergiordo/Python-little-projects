be07bfecf5ae21233e29b9c2fe196ad9141ab757
Sergio Ordonez Lopez <141227441+Sergiordo@users.noreply.github.com>
2024-06-18 21:58:30 +0700

    Update Volume cone.py

--- Volume cone.py
+++ Volume cone.py
@@ -1,13 +1,11 @@
-def area_cone(radius, height):
+def volume_cone(radius, height):
     pi = 3.14
-    circle = pi * (radius **2)
-    circumference = 2 * pi * radius
-    rectangle = circumference * height
-    area_cylinder = (circle * 2) + rectangle
-    area_cone = area_cylinder / 3
-    return area_cone
+    area_base = pi * (radius **2)
+    volume_cylinder = (area_base) * height
+    volume_cone = volume_cylinder / 3
+    return volume_cone
     
 r = float(input("Enter the radius: "))
 h = float(input("Enter the height: "))
-result = area_cone(r,h)
-print("The total area of the cone is: ", result)
+result = volume_cone(r,h)
+print("The total volume of the cone is: ", result)
